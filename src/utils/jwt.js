6815bf9313ca7eb6bf54c2d5290ab1110bdfb0f7
viitomontunes <viitomontunes@gmail.com>
2026-06-23 02:05:18 -0300

    feat: implementado configuração para jwt e middleware para verificação de token

---
+++ src/utils/jwt.js
@@ -0,0 +1,48 @@
+import jwt from 'jsonwebtoken';
+
+export function generateAccessToken(user) {
+    
+    return jwt.sign(
+        {
+            sub: user.id,
+            companyId: user.companyId,
+            role: user.role,
+            name: user.name
+        },
+        process.env.JWT_ACCESS_SECRET,
+        {
+            expiresIn: process.env.JWT_ACCESS_EXPIRATION,
+        }
+    );
+}
+
+export function generateRefreshToken(user) {
+    
+    return jwt.sign(
+        {
+            sub: user.id,
+        },
+        process.env.JWT_REFRESH_SECRET,
+        {
+            expiresIn: process.env.JWT_REFRESH_EXPIRATION,
+        }
+    );
+}
+
+export function verifyAccessToken(token) {
+
+    return jwt.verify(
+        token,
+        process.env.JWT_ACCESS_SECRET
+    );
+
+}
+
+export function verifyRefreshToken(token) {
+
+    return jwt.verify(
+        token,
+        process.env.JWT_REFRESH_SECRET
+    );
+
+}
