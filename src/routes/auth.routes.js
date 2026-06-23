81ec8d690bfc2a385c48d2686ac3df205fdbbb3e
viitomontunes <viitomontunes@gmail.com>
2026-06-23 01:30:40 -0300

    feat: criado estrutura da rota de autenticação

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import * as authController from '../controllers/auth.controller.js';
+
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.post('/register', authController.register);
+
+router.post('/login', authController.login);
+
+router.post('/refresh', authController.refresh);
+
+router.use(authMiddleware); 
+
+router.post('/logout', authController.logout);
+
+router.get('/me', authController.me);
+
+export default router;
