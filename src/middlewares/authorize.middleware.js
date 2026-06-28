85df499d9e2c6ba5cebef437a503c3ce5308c4c7
viitomontunes <viitomontunes@gmail.com>
2026-06-28 15:59:32 -0300

    fix: corrigido bug no middleware de autorização

--- src/middlewares/authorize.middleware.js
+++ src/middlewares/authorize.middleware.js
@@ -2,20 +2,23 @@ export function authorize() {
   return (req, res, next) => {
     if (!req.user) {
       return res.status(401).json({
-        message: "Usuário não autenticado."
+        message: "Usuário não autenticado.",
       });
     }
 
+    const role = (req.user.role || "").toLowerCase();
+    const department = (req.user.departmentName || "").toLowerCase();
+
     const isAdminDepartment =
-      req.user.departmentName === "Administração";
+      department === "administração".toLowerCase();
 
     const isManager =
-      req.user.role === "Gerente";
+      role === "gerente".toLowerCase();
 
     if (!isAdminDepartment || !isManager) {
       return res.status(403).json({
         message:
-          "Acesso permitido apenas para usuários do departamento de Administração com cargo de Gerente."
+          "Acesso permitido apenas para usuários do departamento de Administração com cargo de Gerente.",
       });
     }
 
